62e5c7f395168640b3bfeb4983df6a7ee8771acf
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-05-19 11:07:26 -0400

    Fixed topo

--- topo/myTopo.py
+++ topo/myTopo.py
@@ -7,21 +7,19 @@ class MyTopo(Topo):
         # Initialize topology
         Topo.__init__(self)
         # Add hosts and switches
-        h1 = self.addHost('h1')    #Internet host
-        h2 = self.addHost('h2')    #Lan host
-        h3 = self.addHost('h3')    #DMZ host
-        h4 = self.addHost('h4')    #Honeypot host
+        h_inet = self.addHost('h_inet')    
+        h_dmz = self.addHost('h_dmz')
+        h_hp = self.addHost('h_hp')
+        h_lan = self.addHost('h_lan')
 
-        s1 = self.addSwitch('s1')    #Firewall + snort switch
-        s2 = self.addSwitch('s2')    #Firewall2 switch
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
 
         # Add (bidirectional) links from host to switches
-        self.addLink(h1, s1)
-        self.addLink(h2, s2)
-        self.addLink(h3, s1)
-        self.addLink(h3, s2)
-        self.addLink(h4, s1)
-        self.addLink(h4, s2)
+        self.addLink(h_inet, s1)
+        self.addLink(h_lan, s2)
+        self.addLink(h_dmz, s1)
+        self.addLink(h_hp, s1)
 
         # Add (bidirectional) links between switches
         self.addLink(s1, s2)
